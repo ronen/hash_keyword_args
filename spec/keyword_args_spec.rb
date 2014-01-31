2e552ce1359eec57d3fbf2133ab0c0086b60c981
ronen barzel <ronen@barzel.org>
2014-01-31 21:14:46 +0000

    update rspec syntax

--- spec/keyword_args_spec.rb
+++ spec/keyword_args_spec.rb
@@ -3,20 +3,20 @@ require 'spec_helper'
 describe "keyword_args" do
 
   it "unadorned args should be optional" do
-    expect { @args = {:a => 17}.keyword_args(:a, :b) }.should_not raise_error
+    expect { @args = {:a => 17}.keyword_args(:a, :b) }.to_not raise_error
     @args.a.should == 17
     @args.b.should be_nil
   end
 
   it ":optional args should be optional" do
-    expect { @args = {:a => 17}.keyword_args(:a => :optional, :b => :optional) }.should_not raise_error
+    expect { @args = {:a => 17}.keyword_args(:a => :optional, :b => :optional) }.to_not raise_error
     @args.a.should == 17
     @args.b.should be_nil
   end
 
   it ":required args should be required" do
-    expect { @args = {:a => 17}.keyword_args(:a => :required, :b => :required) }.should raise_error(ArgumentError)
-    expect { @args = {:a => 17, :b => 32}.keyword_args(:a => :required, :b => :required) }.should_not raise_error(ArgumentError)
+    expect { @args = {:a => 17}.keyword_args(:a => :required, :b => :required) }.to raise_error(ArgumentError)
+    expect { @args = {:a => 17, :b => 32}.keyword_args(:a => :required, :b => :required) }.to_not raise_error
     @args.a.should == 17
     @args.b.should == 32
   end
@@ -28,42 +28,42 @@ describe "keyword_args" do
   end
 
   it "should complain about invalid args" do
-    expect { @args = {:a => "whatever"}.keyword_args(:b) }.should raise_error(ArgumentError)
+    expect { @args = {:a => "whatever"}.keyword_args(:b) }.to raise_error(ArgumentError)
   end
 
   it "should not complain about invalid args if :OTHERS is given" do
-    expect { @args = {:a => "whatever"}.keyword_args(:b) }.should raise_error(ArgumentError)
-    expect { @args = {:a => "whatever"}.keyword_args(:b, :OTHERS) }.should_not raise_error
+    expect { @args = {:a => "whatever"}.keyword_args(:b) }.to raise_error(ArgumentError)
+    expect { @args = {:a => "whatever"}.keyword_args(:b, :OTHERS) }.to_not raise_error
     @args[:a].should == "whatever"
   end
 
   it "should not define method for OTHERS arg" do
     @args = {:a => "whatever"}.keyword_args(:b, :OTHERS)
-    expect { @args.a }.should raise_error(NoMethodError)
+    expect { @args.a }.to raise_error(NoMethodError)
     @args[:a].should == "whatever"
   end
 
   it "should validate against list of values" do
-    expect { @args = {:a => :BAD}.keyword_args(:a => [:OK, :GOOD]) }.should raise_error(ArgumentError)
-    expect { @args = {:a => :OK}.keyword_args(:a => [:OK, :GOOD]) }.should_not raise_error
+    expect { @args = {:a => :BAD}.keyword_args(:a => [:OK, :GOOD]) }.to raise_error(ArgumentError)
+    expect { @args = {:a => :OK}.keyword_args(:a => [:OK, :GOOD]) }.to_not raise_error
     @args.a.should == :OK
   end
 
   it "should validate against class" do
-    expect { @args = {:a => "hello"}.keyword_args(:a => Integer) }.should raise_error(ArgumentError)
-    expect { @args = {:a => 17}.keyword_args(:a => Integer) }.should_not raise_error
+    expect { @args = {:a => "hello"}.keyword_args(:a => Integer) }.to raise_error(ArgumentError)
+    expect { @args = {:a => 17}.keyword_args(:a => Integer) }.to_not raise_error
     @args.a.should == 17
   end
 
   it "should allow validity set via hash" do
-    expect { @args = {:a => "hello"}.keyword_args(:a => {:valid => Integer}) }.should raise_error(ArgumentError)
-    expect { @args = {:a => 17}.keyword_args(:a => {:valid => Integer}) }.should_not raise_error
+    expect { @args = {:a => "hello"}.keyword_args(:a => {:valid => Integer}) }.to raise_error(ArgumentError)
+    expect { @args = {:a => 17}.keyword_args(:a => {:valid => Integer}) }.to_not raise_error
     @args.a.should == 17
   end
 
   it "should allow nil if :allow_nil is set" do
-    expect { @args = {:a => nil}.keyword_args(:a => {:valid => Integer}) }.should raise_error(ArgumentError)
-    expect { @args = {:a => nil}.keyword_args(:a => {:valid => Integer, :allow_nil => true}) }.should_not raise_error
+    expect { @args = {:a => nil}.keyword_args(:a => {:valid => Integer}) }.to raise_error(ArgumentError)
+    expect { @args = {:a => nil}.keyword_args(:a => {:valid => Integer, :allow_nil => true}) }.to_not raise_error
     @args.a.should be_nil
   end
 
@@ -88,9 +88,9 @@ describe "keyword_args" do
   end
 
   it "should validate if :enumerable and :valid" do
-    expect {@args = {:a => 3}.keyword_args(:a => { :enumerable => true, :valid => String })}.should raise_error(ArgumentError)
-    expect {@args = {:a => ["OK", 3]}.keyword_args(:a => { :enumerable => true, :valid => String })}.should raise_error(ArgumentError)
-    expect {@args = {:a => ["OK", "YY"]}.keyword_args(:a => { :enumerable => true, :valid => String })}.should_not raise_error
+    expect {@args = {:a => 3}.keyword_args(:a => { :enumerable => true, :valid => String })}.to raise_error(ArgumentError)
+    expect {@args = {:a => ["OK", 3]}.keyword_args(:a => { :enumerable => true, :valid => String })}.to raise_error(ArgumentError)
+    expect {@args = {:a => ["OK", "YY"]}.keyword_args(:a => { :enumerable => true, :valid => String })}.to_not raise_error
     @args.a.should == ["OK", "YY"]
   end
 
